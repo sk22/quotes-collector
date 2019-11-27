4f2d22ee4999dc79c3eab3bd48e452f91aa37578
Samuel Kaiser <samuel.kaiser01@gmail.com>
2019-11-27 23:21:35 +0100

    Introduce SQLite database

---
+++ backend/ddl/ddl.sql
@@ -0,0 +1,19 @@
+create table quotes (
+  q_id integer primary key autoincrement,
+  q_text text not null,
+  q_user integer not null,
+  q_author text not null,
+  q_votes integer not null default 0
+);
+
+create table users (
+  u_id integer primary key autoincrement,
+  u_username text not null
+);
+
+create table votes (
+  v_quote integer not null,
+  v_user integer not null,
+  v_vote boolean not null,
+  primary key (v_quote, v_user)
+);
--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,12 @@
 const express = require('express')
 const { readFileSync } = require('fs')
 
+const { Database } = require('sqlite3').verbose()
+
+const db = new Database('./data/quotes.db', err => {
+  if (err) console.error(err)
+})
+
 const app = express()
 
 const quotes = JSON.parse(readFileSync('./quotes.json'))
