3dfd42e9c1ae616e31c85036d4249e02806b6573
Samuel Kaiser <samuel.kaiser01@gmail.com>
2019-12-15 17:44:19 +0100

    Create GET backend for every table

--- backend/index.js
+++ backend/index.js
@@ -1,18 +1,42 @@
 const express = require('express')
-const { readFileSync } = require('fs')
 
 const { Database } = require('sqlite3').verbose()
+const app = express()
+
+const selectAllFromTable = (table, links) => new Promise((resolve, reject) => {
+  db.all(`SELECT * FROM ${table}${links
+      ? ` INNER JOIN ${links.join(', ')}` : ''};`, (err, rows) => {
+    if (err) {
+      reject(err)
+    } else {
+      resolve(rows)
+    }
+  })
+})
 
 const db = new Database('./data/quotes.db', err => {
   if (err) console.error(err)
+
+  const tables = [
+    { name: 'quotes',
+      fields: ['q_id', 'q_text', 'q_user', 'q_author', 'q_votes' ],
+      links: ['users on q_user = u_id'] },
+    { name: 'users', fields: ['u_id', 'u_username' ]},
+    { name: 'votes',
+      fields: ['v_quote', 'v_user', 'v_vote' ],
+      links: ['users on v_user = u_id', 'quotes on v_quote = q_id'] }
+  ]
+
+  tables.forEach(table => {
+    app.get(`/api/${table.name}`, (_, res, next) => {
+      rows = selectAllFromTable(table.name, table.links)
+        .then(rows => res.send(rows))
+        .catch(next)
+    })
+  })
+    
+  app.listen(3001)
+  console.log('listening on http://localhost:3001')
 })
 
-const app = express()
 
-const quotes = JSON.parse(readFileSync('./quotes.json'))
-
-app.get('/api/quotes', (req, res) => {
-  res.json(quotes)
-})
-
-app.listen(3001)
